d8d7999af5a5a8b039aa84578636976ce29b95a6
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-09-29 14:30:48 +0530

    Create  Equal Row and Column Pairs.java

---
+++ Medium/Equal Row and Column Pairs.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        
+ int n = grid.length;
+        int [][] flip = new int[n][n];
+        
+        transpose(grid,flip);
+        
+        int count = 0;
+        
+        for(int i = 0; i<n ; i++){
+            for(int j = 0; j<n; j++){
+                if(Arrays.equals(grid[i],flip[j]))
+                    count++;
+            }
+        }
+        
+        return count;
+        
+    }
+    public void transpose(int [][] src , int [][] target){
+        for(int  i = 0; i<src.length; i++){
+            for(int j = 0; j<src[0].length; j++){
+                target[i][j] = src[j][i];
+            }
+        }
+    
+    }
+}
