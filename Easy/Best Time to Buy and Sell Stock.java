1374f234f2782e09205d47bd0b6131dc08909a04
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-10-31 08:42:51 +0530

    Create Best Time to Buy and Sell Stock.java

---
+++ Easy/Best Time to Buy and Sell Stock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxp = 0;
+        int minp = Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++)
+        {
+            minp = Math.min(minp,prices[i]);
+            maxp = Math.max(maxp,(prices[i]-minp));
+        }
+        return maxp;
+        
+    }
+}
