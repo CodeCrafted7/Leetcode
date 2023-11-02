0228df0b8c614bc9e1e3b2aba0915276fb8908d6
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-11-02 07:41:17 +0530

    Create  Find Mode in Binary Search Tree.java

---
+++ Easy/Find Mode in Binary Search Tree.java
@@ -0,0 +1,30 @@
+class Solution {
+    private Map<Integer, Integer> hashMap = new HashMap<>();
+    private int max = 1;
+    public int[] findMode(TreeNode root) {
+        if(root == null){
+            return new int[0];
+        }
+        helper(root);
+        int result[] = new int[hashMap.size()]; int i = 0;
+        for(Integer k: hashMap.keySet()){
+            if(hashMap.get(k) == max)
+                result[i++] = k;
+        }
+        return Arrays.copyOf(result, i);
+    }
+    
+    private void helper(TreeNode root){
+        if(root != null){
+            if(hashMap.containsKey(root.val)){
+                int count = hashMap.get(root.val) + 1;
+                hashMap.put(root.val, hashMap.get(root.val) + 1);
+                max = Math.max(max, count);
+            }
+            else
+                hashMap.put(root.val, 1);
+            helper(root.left);
+            helper(root.right);
+        }
+    }
+}
