02b773d6babe0231736f783ccc45e0e99f3b19d9
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-12-12 08:07:54 +0530

    Create Element Appearing More Than 25% In Sorted Array.java

---
+++ Easy/Element Appearing More Than 25% In Sorted Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+    int n  =arr.length;
+    int c = 0;
+    if(n==1){
+        return arr[0];
+    }
+    for(int i=0;i<n-n/4;i++){
+    if(arr[i]==arr[i+n/4]){
+        c = arr[i];
+    }
+    }
+    return c;    
+    }
+}
