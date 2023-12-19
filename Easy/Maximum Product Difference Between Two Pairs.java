62cfc6c88ccd80e41a11b1ed825aed653bdf8fa6
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-12-19 10:21:47 +0530

    Create Maximum Product Difference Between Two Pairs.java

---
+++ Easy/Maximum Product Difference Between Two Pairs.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ab = nums[0] * nums[1];
+        int cd = nums[n-1] * nums[n-2];
+        return cd - ab;    
+    }
+}
