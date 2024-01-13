1222caa36e2c5f27255ef68bb61e4fd7293dfe1f
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-01-13 12:55:56 +0530

    Create Binary Tree Tilt.java

---
+++ Easy/Binary Tree Tilt.java
@@ -0,0 +1,21 @@
+class Solution {
+    private int ans=0 ; 
+    private int helper(TreeNode root){
+        if(root==null){return 0; }
+        
+        int l= helper(root.left) ;
+        int r= helper(root.right) ;
+        
+        ans+=Math.abs(l-r) ;
+        
+        return l+r+root.val; 
+        
+    }
+    public int findTilt(TreeNode root) {
+       
+         helper(root) ;
+        
+         return ans; 
+        
+    }
+}
