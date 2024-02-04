ae81517f7373de5a6a8d1cf4f6f4d418631e31de
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-02-04 18:35:44 +0530

    Create Positions of Large Groups.java\

---
+++ Easy/Positions of Large Groups.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<List<Integer>> largeGroupPositions(String s) {
+        List<List<Integer>> ans=new ArrayList<>();
+        int start=0,n=s.length();
+        for(int i=1;i<=n;i++){
+            if((i==n) || !(s.charAt(i)==s.charAt(i-1))){
+                if((i-start >=3)) ans.add(List.of(start,(i-1)));
+                start=i;
+            }
+        }
+        return ans;
+    }
+}
