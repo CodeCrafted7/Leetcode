8105808700ee91e9f210bafe49b88d768a979b56
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-03-03 09:54:12 +0530

    Create _Squares of a Sorted Array.java

---
+++ Easy/_Squares of a Sorted Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        
+        int[] answer = new int[nums.length];
+        int i = 0, j = nums.length - 1, k = nums.length - 1;
+        
+        while (k >= 0) {
+            if (Math.abs (nums[i]) > Math.abs (nums[j])) {
+                answer[k--] = nums[i] * nums[i++];
+            }
+            else {
+                answer[k--] = nums[j] * nums[j--];
+            }
+        }
+        
+        return answer;
+    }
+}
