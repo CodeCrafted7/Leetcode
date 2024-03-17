a1a33e03ecaae455639fd8787eb20abcea4e4eed
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-03-17 16:27:30 +0530

    Create Find Lucky Integer in an Array.java

---
+++ Easy/Find Lucky Integer in an Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        int temp[] = new int[501];
+        for(int i : arr) temp[i]++;
+        int ans = -1;
+        for(int i=1; i<temp.length; i++){
+            if(i == temp[i]) ans = i;
+        }
+        return ans; 
+    }
+}
