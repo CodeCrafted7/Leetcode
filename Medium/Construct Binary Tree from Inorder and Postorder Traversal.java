d508fe984344ea43a6a33fe67a9975b0136dc0d6
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-04-11 09:38:51 +0530

    Create Construct Binary Tree from Inorder and Postorder Traversal.java

---
+++ Medium/Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int idx;
+    
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder.length != postorder.length) return null;
+        if (inorder.length == 0) return null;
+        idx = postorder.length-1;
+        TreeNode root = build(inorder, postorder, 0, idx);
+        return root;
+    }
+    
+    private TreeNode build(int[] inorder, int[] postorder, int start, int end) {
+        if (start>end) return null;
+        TreeNode node = new TreeNode(postorder[idx--]);
+        if (start==end) return node;
+        
+        int index = findIdx(inorder, node.val, end);
+        node.right = build(inorder, postorder, index+1, end);
+        node.left = build(inorder, postorder, start, index-1);
+        return node;
+    }
+    
+    private int findIdx(int[] inorder, int val, int end) {
+        for (int i=end; i>=0; i--) {
+            if (inorder[i]==val) return i;
+        }
+        return 0;
+    }
+}
