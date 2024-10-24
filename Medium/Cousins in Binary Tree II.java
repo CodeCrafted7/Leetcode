e5b965c810c489e28db49db3eb0a3f057bbad739
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-10-24 14:40:31 +0530

    Create  Cousins in Binary Tree II.java

---
+++ Medium/Cousins in Binary Tree II.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode replaceValueInTree(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        int prev = root.val;
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            int curr = 0; 
+            while(size>0){
+                TreeNode temp = q.poll();
+                int lr = (temp.left != null? temp.left.val:0) + (temp.right != null? temp.right.val:0);
+                if(temp.left != null){
+                    temp.left.val = lr;
+                    q.add(temp.left);
+                }
+
+                if(temp.right != null){
+                    temp.right.val = lr;
+                    q.add(temp.right);
+                }
+                curr += lr;
+                temp.val = prev-temp.val;
+                size--;
+            }
+            prev = curr;
+        }
+        return root; 
+    }
+}
