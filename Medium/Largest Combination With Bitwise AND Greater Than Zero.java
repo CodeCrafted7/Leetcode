1731bb0adf4019defafedba11ae6c5d026f27e2f
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-11-08 10:58:17 +0530

    Create Largest Combination With Bitwise AND Greater Than Zero.java

---
+++ Medium/Largest Combination With Bitwise AND Greater Than Zero.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int largestCombination(int[] candidates) {
+        int max = 0;
+
+        for(int i=0; i<=24; i++){
+            int setBits = 0;
+            
+            for(int c : candidates){
+                setBits += (c>>i) & 1;
+                max = Math.max(setBits,max);
+            } 
+        }
+
+        return max;
+    }
+}
